c01883c41fa8f7e1b7221c743ea6f38854318d30
jmgalindor9802 <jmgalindor9802@gmail.com>
2025-03-01 12:47:54 -0500

    Actualiacion de Estudiante Entity

--- api/src/users/entities/estudiante.entity.ts
+++ api/src/users/entities/estudiante.entity.ts
@@ -1,34 +1,53 @@
-import { Check, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { 
+  Check, 
+  Column, 
+  Entity, 
+  Index, 
+  ManyToOne, 
+  OneToMany, 
+  PrimaryGeneratedColumn 
+} from 'typeorm';
+import { Clase } from '../../classes/entities/clase.entity';
+import { Pais } from '../../places/entities/pais.entity';
 
-import { Clase } from '../../curso/entities/clase.entity';
-
-@Entity()
+@Entity({ name: 'ESTUDIANTE' }) // Nombre de la tabla en la BD
 export class Estudiante {
   @PrimaryGeneratedColumn({
     primaryKeyConstraintName: 'PK_ESTUDIANTE',
     comment: 'Clave primaria del Estudiante',
   })
   pk_estudiante: number;
-  @Column({
-    type: 'nvarchar',
-    length: 20,
-    comment: 'Documento de indentificacion del Estudiante',
-  })
+
+  @Column({ type: 'nvarchar', length: 20, nullable: false, comment: 'Documento de identificación del Estudiante' })
   @Check('CK_DOC_IDENTIDAD_ESTUDIANTE', "DOC_IDENTIDAD='0' OR (ISNUMERIC(DOC_IDENTIDAD)=1 AND LEN(DOC_IDENTIDAD)>=6 AND LEN(DOC_IDENTIDAD)<=12)")
+  @Index('IX_DOC_IDENTIDAD_ESTUDIANTE', { unique: false }) // Índice no único
   doc_identidad: string;
-  @Column({ type: 'nvarchar', length: 50, comment: 'Nombre del Estudiante' })
+
+  @Column({ type: 'nvarchar', length: 50, nullable: false, comment: 'Nombre del Estudiante' })
   nombre: string;
-  @Column({ type: 'nvarchar', length: 50, comment: 'Apellido del Estudiante' })
+
+  @Column({ type: 'nvarchar', length: 50, nullable: false, comment: 'Apellido del Estudiante' })
   apellido: string;
-  @Column({ type: 'nvarchar', length: 100, comment: 'Correo del Estudiante' })
+
+  @Column({ type: 'nvarchar', length: 100, nullable: false, comment: 'Correo del Estudiante' })
   correo: string;
-  @Column({ type: 'nvarchar', length: 60, comment: 'Usuario del Estudiante' })
+
+  @Column({ type: 'nvarchar', length: 60, nullable: false, comment: 'Usuario del Estudiante' })
   usuario: string;
-  @Column({ type: 'nvarchar', length: 50, nullable: true, 'Numero de contacto del Estudiante'})
-  @Check('CK_NUM_CONTACTO_ESTUDIANTE',"NUM_CONTACTO='0' OR LEN(NUM_CONTACTO)>=7")
+
+  @Column({ type: 'nvarchar', length: 50, nullable: false, comment: 'Número de contacto del Estudiante' })
+  @Check('CK_NUM_CONTACTO_ESTUDIANTE', "NUM_CONTACTO='0' OR LEN(NUM_CONTACTO)>=7")
   num_contacto: string;
-  @Column({ type: 'bit', default: 1 })
+
+  @Column({ type: 'bit', nullable: false, default: () => '1', comment: 'Estado de registro del estudiante' })
   registrado: boolean;
+
+  @Column({ type: 'nvarchar', length: 50, nullable: false, comment: 'Tipo de documento del Estudiante' })
+  tipo_doc: string;
+
+  @ManyToOne(() => Pais, (pais) => pais.estudiantes, { nullable: false })
+  @Column({ type: 'int', nullable: false, comment: 'Clave foránea a la tabla PAIS', name: 'FK_PAIS' })
+  fk_pais: number;
 
   @OneToMany(() => Clase, (clase) => clase.estudiante)
   clases: Clase[];
