905074bd43130d26dbebf84d1a6cc4cdae5b787e
jmgalindor9802 <jmgalindor9802@gmail.com>
2025-03-01 14:54:04 -0500

    Actualizacion de Pregunta Entity

--- api/src/surveys/entities/pregunta.entity.ts
+++ api/src/surveys/entities/pregunta.entity.ts
@@ -5,28 +5,33 @@ import {
   OneToMany,
   Check,
 } from 'typeorm';
-
 import { Encuesta } from './encuesta.entity';
 
-@Entity()
-@Check('ck_estado_pregunta', "estado='Deshabilitado' OR estado='Habilitado'")
+@Entity({ name: 'PREGUNTA' })
+@Check('CK_ESTADO_PREGUNTA', "ESTADO='Deshabilitado' OR ESTADO='Habilitado'")
 export class Pregunta {
-  @PrimaryGeneratedColumn({ comment: 'Clave primaria de la Pregunta' })
+  @PrimaryGeneratedColumn({ name: 'PK_PREGUNTA', comment: 'Clave primaria de la Pregunta' })
   pk_pregunta: number;
-  @Column({ type: 'nvarchar', length: 500, comment: 'Texto de la Pregunta' })
+
+  @Column({ name: 'PREGUNTA', type: 'nvarchar', length: 'MAX', nullable: false, comment: 'Texto de la Pregunta' })
   pregunta: string;
-  @Column({ type: 'nvarchar', length: 50, comment: 'Categoría de la Pregunta' })
+
+  @Column({ name: 'CATEGORIA', type: 'nvarchar', length: 50, nullable: false, comment: 'Categoría de la Pregunta' })
   categoria: string;
-  @Column({ type: 'nvarchar', length: 50, comment: 'Tipo de la Pregunta' })
+
+  @Column({ name: 'TIPO', type: 'nvarchar', length: 50, nullable: false, comment: 'Tipo de la Pregunta' })
   tipo: string;
+
   @Column({
+    name: 'ESTADO',
     type: 'nvarchar',
     length: 50,
-    comment:
-      "Si el estado es 'Habilitado' la Pregunta se podrá mostrar en la Encuesta",
+    nullable: false,
+    comment: "Si el estado es 'Habilitado' la Pregunta se podrá mostrar en la Encuesta",
   })
   estado: string;
-  @Column({ type: 'int', nullable: true, comment: '' })
+
+  @Column({ name: 'ORDEN', type: 'int', nullable: true, comment: 'Orden de la Pregunta' })
   orden: number;
 
   @OneToMany(() => Encuesta, (encuesta) => encuesta.pregunta)
