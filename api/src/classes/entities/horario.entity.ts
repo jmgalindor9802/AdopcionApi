f1156d8a49264902f65408573e976e91fec0f00a
jmgalindor9802 <jmgalindor9802@gmail.com>
2025-03-01 14:47:17 -0500

    Actualizacion Horario Entity

--- api/src/classes/entities/horario.entity.ts
+++ api/src/classes/entities/horario.entity.ts
@@ -9,17 +9,20 @@ import {
 
 import { Grupo } from './grupo.entity';
 
-@Entity()
-@Check('ck_hora_inicio_fin', 'hora_fin>hora_inicio')
+@Entity({ name: 'HORARIO' })
+@Check('CK_HORA_INICIO_FIN', 'HORA_FIN > HORA_INICIO')
 export class Horario {
-  @PrimaryGeneratedColumn({ comment: 'Clave primaria de Horario' })
+  @PrimaryGeneratedColumn({ name: 'PK_HORARIO', comment: 'Clave primaria de Horario' })
   pk_horario: number;
-  @Column({ type: 'date', comment: 'Fecha del Horario' })
+
+  @Column({ name: 'FECHA', type: 'date', nullable: false, comment: 'Fecha del Horario' })
   fecha: Date;
-  @Column({ type: 'time', comment: 'Hora de Inicio' })
-  hora_inicio: Date;
-  @Column({ type: 'time', comment: 'Hora de finalización' })
-  hora_fin: Date;
+
+  @Column({ name: 'HORA_INICIO', type: 'time', nullable: false, comment: 'Hora de Inicio' })
+  hora_inicio: string;
+
+  @Column({ name: 'HORA_FIN', type: 'time', nullable: false, comment: 'Hora de Finalización' })
+  hora_fin: string;
 
   @ManyToOne(() => Grupo, (grupo) => grupo.horarios)
   @JoinColumn({
