1490c0b216c5ede4322c06064541c6651c3f64b1
jmgalindor9802 <jmgalindor9802@gmail.com>
2025-03-02 15:07:41 -0500

    Correcion en conexion a base de datos local

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -3,6 +3,23 @@ import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
 import { environments } from './environments';
+import { Certificado } from './certificates/entities/certificado.entity';
+import { Pais } from './places/entities/pais.entity';
+import { Clase } from './classes/entities/clase.entity';
+import { Curso } from './classes/entities/curso.entity';
+import { Grupo } from './classes/entities/grupo.entity';
+import { Horario } from './classes/entities/horario.entity';
+import { TipoGrupo } from './classes/entities/tipoGrupo.entity';
+import { Empresa } from './companies/entities/empresa.entity';
+import { Sector } from './companies/entities/sector.entity';
+import { Salon } from './places/entities/salon.entity';
+import { Ubicacion } from './places/entities/ubicacion.entity';
+import { Administrador } from './users/entities/administrador.entity';
+import { EsriAcademy } from './users/entities/esri.entity';
+import { Estudiante } from './users/entities/estudiante.entity';
+import { Instructor } from './users/entities/instructor.entity';
+import { Encuesta } from './surveys/entities/encuesta.entity';
+import { Pregunta } from './surveys/entities/pregunta.entity';
 
 // Asegurar que NODE_ENV tenga un valor por defecto
 const currentEnv = process.env.NODE_ENV || 'development';
@@ -27,8 +44,16 @@ const source = new DataSource({
   },
   dropSchema: false,
   synchronize: true,
-  entities: [join(__dirname, '**', '**', '*.entity.{ts,js}')],
-  migrations: [join(__dirname, 'database', 'migrations', '*.ts')]
+  logging: true,
+  entities: [
+    Pais, Ubicacion, Sector,
+    Empresa, Salon,
+    Grupo, TipoGrupo, Curso, Instructor, Administrador, EsriAcademy,
+    Estudiante, 
+    Clase, Horario,
+    Certificado, Encuesta,Pregunta
+  ],
+  migrations: [join(__dirname, 'databases', 'migrations', '*.ts')]
 });
 
 export default source;
